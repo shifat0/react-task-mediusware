0a265b8c8bf017b26d6ae5baddb9554d2e48f96d
Md. Shoaib Akhter Shifat <sifuisu3@gmail.com>
2023-12-23 22:59:35 +0600

    added infinity scroll

--- src/components/All-Contacts.jsx
+++ src/components/All-Contacts.jsx
@@ -1,24 +1,62 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 
 const AllContacts = () => {
-  const [contacts, setContacts] = useState({});
-  const [loading, setLoading] = useState(false);
+  const [contacts, setContacts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [showMore, setShowMore] = useState(true);
+
+  // ref to the target element
+  const loaderRef = useRef(null);
+
+  // fetching all-contacts
+  const fetchContacts = async () => {
+    try {
+      const res = await axios.get(
+        `https://contact.mediusware.com/api/contacts/?page=${page}`
+      );
+      console.log(res.data.results);
+
+      setContacts((prev) => [...prev, ...res.data.results]);
+      setPage((prevPage) => prevPage + 1);
+    } catch (err) {
+      // if (err.message.includes(404)) setShowMore(false);
+      if (err.response.status === 404) setShowMore(false);
+      // console.log(err.message);
+    }
+  };
+
+  // handling observer callback
+  const handleObserver = (entries) => {
+    const target = entries[0];
+    // fetching contacts when target isIntersecting true
+    if (target.isIntersecting) {
+      fetchContacts();
+    }
+  };
 
   useEffect(() => {
-    const fetchContacts = async () => {
-      setLoading(true);
-      const contacts = await axios.get(
-        "https://contact.mediusware.com/api/contacts/"
-      );
-      setContacts(contacts.data);
-      setLoading(false);
+    // observer options
+    const options = {
+      root: null,
+      rootMargin: "20px",
+      threshold: 1.0,
     };
 
-    fetchContacts();
-  }, [setContacts]);
+    // observing the users viewport
+    const observer = new IntersectionObserver(handleObserver, options);
+    // starting observer
+    if (loaderRef.current) {
+      observer.observe(loaderRef.current);
+    }
 
-  // contacts.results.map((c) => console.log(c));
+    // closing observer
+    return () => {
+      if (loaderRef.current) {
+        observer.unobserve(loaderRef.current);
+      }
+    };
+  }, [contacts]);
 
   return (
     <div>
@@ -30,17 +68,22 @@ const AllContacts = () => {
             <th scope="col">Number</th>
             <th scope="col">Country</th>
           </tr>
-          {loading
-            ? "Loading..."
-            : contacts?.results?.map((contact) => (
-                <tr key={contact.id}>
-                  <td>{contact.id}</td>
-                  <td>{contact.phone}</td>
-                  <td>{contact.country.name}</td>
-                </tr>
-              ))}
         </thead>
+        <tbody>
+          {contacts?.map((contact) => (
+            <tr key={contact.id}>
+              <td>{contact.id}</td>
+              <td>{contact.phone}</td>
+              <td>{contact.country.name}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
+      {showMore && (
+        <div ref={loaderRef} style={{ margin: "10px" }}>
+          Loading...
+        </div>
+      )}
     </div>
   );
 };
